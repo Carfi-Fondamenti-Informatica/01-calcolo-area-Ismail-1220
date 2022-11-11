286da25aa683e9fd019772fd701e4cc874624113
Ismail-1220 <118005408+Ismail-1220@users.noreply.github.com>
2022-11-11 19:01:56 +0100

    Update ismail.cpp

--- ismail.cpp
+++ ismail.cpp
@@ -1,19 +1,17 @@
-# 01-calcolo-area-Ismail-1220
-01-calcolo-area-Ismail-1220 created by GitHub Classroom
+//# 01-calcolo-area-Ismail-1220
+//01-calcolo-area-Ismail-1220 created by GitHub Classroom
 #include <iostream>
 using namespace std;
-int main()
-{
-    //Qui vanno definite le varriabili
-    int a, b, c;
-    cin >> a >> b >> c;
+int main(){
+//Qui vanno definite le varriabili
+float a, b, c;
+
+cin >> a >> b >> c;
     
-    //Qui va scritto il codice per eseguire i calcoli richiesti
-#include <iostream>
-using namespace std;
-int main()
-    cout <<(a*b)/2 <<endl;
-    cout <<a*a <<endl;
-    cout <<a*b <<endl;
-    cout <<((a+b)/2)*c<<endl;
+//Qui va scritto il codice per eseguire i calcoli richiesti
+
+cout << (a*b)/2 <<endl;
+cout << a*a <<endl;
+cout << a*b <<endl;
+cout << ((a+b)/2)*c <<endl;
 }
