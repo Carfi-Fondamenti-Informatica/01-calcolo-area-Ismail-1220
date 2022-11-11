946ad330b01ab7b7a8381545c82611e15d5cc099
Ismail-1220 <118005408+Ismail-1220@users.noreply.github.com>
2022-11-11 19:10:37 +0100

    Update ismail.cpp

--- ismail.cpp
+++ ismail.cpp
@@ -4,12 +4,11 @@
 using namespace std;
 int main(){
 //Qui vanno definite le varriabili
-
-cin >> a >> b >> c;
+    float a, b, c;
+    cin >> a >> b >> c;
     
 //Qui va scritto il codice per eseguire i calcoli richiesti
-    float a=0 , b=0, c=0;
-    cin >> a >> b >> c;
+
     cout << (a*b)/2 <<endl;
     cout << a*a <<endl;
     cout << a*b <<endl;
